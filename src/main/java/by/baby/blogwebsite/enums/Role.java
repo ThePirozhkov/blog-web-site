1df58c1b07f8773732488543c110d55d8d8e43fe
ThePirozhkov <igromansers@gmail.com>
2024-11-22 19:42:13 +0300

    - Добавлен Entity пользователя и роли

---
+++ src/main/java/by/baby/blogwebsite/enums/Role.java
@@ -0,0 +1,5 @@
+package by.baby.blogwebsite.enums;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/by/baby/blogwebsite/persistence/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package by.baby.blogwebsite.persistence.entity;
+
+import by.baby.blogwebsite.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    public UserEntity(Role role, String email, String password, String username) {
+        this.role = role;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+}
