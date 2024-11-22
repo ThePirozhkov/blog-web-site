4f4a4c3244e89733545a5c013f3aa586bbd1fc4a
ThePirozhkov <igromansers@gmail.com>
2024-11-22 18:57:26 +0300

    INITIAL COMMIT

---
+++ src/main/java/by/baby/blogwebsite/BlogWebSiteApplication.java
@@ -0,0 +1,13 @@
+package by.baby.blogwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogWebSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/baby/blogwebsite/BlogWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package by.baby.blogwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
