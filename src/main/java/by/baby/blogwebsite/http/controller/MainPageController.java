a478844294b59314791001b0fbde266870f51b6b
ThePirozhkov <igromansers@gmail.com>
2024-11-25 13:50:09 +0300

    Добавлена кнопка для перехода в аккаунт пользователя

--- src/main/java/by/baby/blogwebsite/http/controller/MainPageController.java
+++ src/main/java/by/baby/blogwebsite/http/controller/MainPageController.java
@@ -1,6 +1,10 @@
 package by.baby.blogwebsite.http.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainPageController {
 
     @GetMapping
-    public String mainPage() {
+    public String mainPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        model.addAttribute("username", userDetails.getUsername());
         return "main/main";
     }
 
