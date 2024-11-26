ca772da501d1af94f6da76196f5b87217eada0c3
ThePirozhkov <igromansers@gmail.com>
2024-11-26 23:22:31 +0300

    Чистка мусора

--- src/main/java/by/baby/blogwebsite/configuration/WebSecurityConfiguration.java
+++ src/main/java/by/baby/blogwebsite/configuration/WebSecurityConfiguration.java
@@ -1,19 +1,14 @@
 package by.baby.blogwebsite.configuration;
 
 import by.baby.blogwebsite.enums.Role;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
